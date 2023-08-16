51e495ff8bff730512ade5b3f5e625e7bc48f10f
Megatronhehee <satyamahendra09@gmail.com>
2023-08-16 20:24:10 +0700

    learn: error handling, loading, fetch api

--- src/pages/ApiSB/ApiSB.jsx
+++ src/pages/ApiSB/ApiSB.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 
+import { BsStarFill } from "react-icons/bs";
+
 const ApiSB = () => {
 	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState("");
 
 	const fetchAPI = () => {
+		setLoading(true);
+		setError("");
+		setData([]);
+
 		fetch("https://dummyjson.com/products")
-			.then((res) => res.json())
-			.then((data) => setData(data.products));
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(`${res.status}: ${res.statusText}`);
+				}
+				return res.json();
+			})
+			.then((data) => setData(data.products))
+			.catch((error) => setError(error.message))
+			.finally(() => setLoading(false));
 	};
 
 	console.log(data);
@@ -17,8 +31,22 @@ const ApiSB = () => {
 	const dataElement =
 		isDataExist &&
 		data.map((product) => (
-			<li key={product.id} className="flex p-2 bg-gray-100 rounded-xl">
+			<li
+				key={product.id}
+				className="relative flex flex-col justify-between flex-shrink-0 w-48 h-full p-2 text-center bg-white rounded-xl"
+			>
+				<img
+					src={product.thumbnail}
+					className="object-cover h-3/4 rounded-xl"
+				/>
+				<h3 className="flex items-center justify-center gap-2 text-sm">
+					<BsStarFill className="text-yellow-400" />
+					{product.rating}
+				</h3>
 				<h2>{product.title}</h2>
+				<p className="absolute top-0 right-0 p-1 text-white bg-gray-800 rounded-md shadow-lg">
+					${product.price}
+				</p>
 			</li>
 		));
 
@@ -26,19 +54,36 @@ const ApiSB = () => {
 		<div className="flex flex-col gap-8">
 			<h1 className="text-2xl">Handle API</h1>
 
-			<section className="flex flex-col gap-4 py-4 border-b">
+			<section className="flex flex-col gap-4 p-4 bg-gray-100 rounded-xl">
 				<h2>Fetch API</h2>
-				<button
-					onClick={fetchAPI}
-					className="px-3 py-1 text-white bg-blue-400 rounded-xl"
-				>
-					fetch
-				</button>
-				<ul className="flex flex-col gap-2">
-					{isDataExist ? (
+
+				<div className="flex gap-4">
+					<button
+						onClick={fetchAPI}
+						className="w-full px-3 py-1 text-white bg-blue-400 rounded-xl"
+						disabled={loading}
+					>
+						Fetch
+					</button>
+					<button
+						onClick={() => setData([])}
+						className="w-full px-3 py-1 text-white bg-blue-300 rounded-xl"
+						disabled={loading}
+					>
+						Clear
+					</button>
+				</div>
+				<ul className="flex gap-2 overflow-auto h-72">
+					{loading ? (
+						<p className="flex items-center justify-center w-full text-gray-400 ">
+							Loading...
+						</p>
+					) : isDataExist ? (
 						dataElement
 					) : (
-						<p className="text-center text-gray-300">display items here</p>
+						<p className="flex items-center justify-center w-full text-gray-400 ">
+							{error || "Display items here"}
+						</p>
 					)}
 				</ul>
 			</section>
