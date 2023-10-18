6d3bc08faac697705d3ff5b24dc93ef2be384943
Megatronhehee <satyamahendra09@gmail.com>
2023-10-18 15:49:57 +0700

    fix: challenge fix

--- src/pages/Challenge2/Challenge2.jsx
+++ src/pages/Challenge2/Challenge2.jsx
@@ -35,36 +35,43 @@ const Challenge2 = () => {
 		</li>
 	));
 
-	console.log(selected);
+	const clear = () => {
+		setSelected({ name: "", url: "" });
+		setSearchInput("");
+	};
 
 	return (
 		<div className="flex flex-col items-center">
 			<h1>Challenge2</h1>
-			{selected.name.length > 0 && (
-				<div className="flex flex-col items-center justify-center mt-8">
+			{selected.name.length > 0 ? (
+				<div className="flex flex-col items-center justify-center h-24 mt-8">
 					<h2 className="text-2xl">{selected.name}</h2>
 					<a href={selected.url} target="_blank">
 						Link
 					</a>
 				</div>
+			) : (
+				<div className="flex flex-col items-center justify-center h-24 mt-8">
+					<h2>no item selected yet</h2>
+				</div>
 			)}
-			<div className="flex mt-4">
+			<div className="relative flex items-center mt-8 w-72">
 				<input
 					type="text"
 					onChange={(e) => setSearchInput(e.target.value)}
 					value={searchInput}
-					placeholder="select..."
+					placeholder="search or select.."
 					onFocus={() => setShowList(true)}
 					onBlur={() =>
 						setTimeout(() => {
 							setShowList(false);
 						}, 100)
 					}
-					className="px-4 py-2 border rounded-lg outline-none w-72"
+					className="w-full px-4 py-2 border rounded-lg outline-none"
 				/>
 				<button
-					onClick={() => setSearchInput("")}
-					className="px-2 py-2 ml-2 text-sm duration-200 border rounded-lg hover:bg-red-300 hover:border-red-300 hover:text-white"
+					onClick={clear}
+					className="absolute px-2 py-2 ml-2 text-sm text-gray-400 duration-200 rounded-md right-2 hover:bg-red-300 hover:border-red-300 hover:text-white"
 				>
 					<PiX />
 				</button>
@@ -72,7 +79,15 @@ const Challenge2 = () => {
 
 			{showList && (
 				<>
-					<ul className="border rounded-lg  w-72">{dataElement}</ul>
+					<ul className="border rounded-lg w-72">
+						{filteredData.length > 0 ? (
+							dataElement
+						) : (
+							<li className="px-4 py-2 text-red-300 cursor-pointer ">
+								no {searchInput} is found
+							</li>
+						)}
+					</ul>
 				</>
 			)}
 		</div>
